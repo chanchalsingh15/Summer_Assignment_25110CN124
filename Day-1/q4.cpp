441088115b2ca2fcfa93250f636f79403a88c986
Chanchal Singh <chanchalsingh2006sr@gmail.com>
2026-06-04 06:34:22 +0530

    Add files via upload

---
+++ Day-1/q4.cpp
@@ -0,0 +1,31 @@
+// Write a program to Count digits in a number
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num, count = 0; 
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if(num == 0){
+
+        count = 1;
+    }
+
+    else {
+        if(num < 0){
+            num = -num;
+        }
+         while(num != 0){
+
+            num = num/10;
+            count++;
+         }
+
+    }
+    cout << " Number of digits: " << count << endl;
+
+    return 0;
+}
